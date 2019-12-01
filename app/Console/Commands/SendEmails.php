d161a40fa2485555a1a5fad5a6b47e6ed78887aa
Pritesh Shinde <priteshs@prdxn.com>
2019-12-01 15:53:14 +0530

    A console command created which sends email to the email mentioned in the command parameter

---
+++ app/Console/Commands/SendEmails.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\UserRegistered;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class SendEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:email {user}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send the test email to the user mentioned in parameter.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Mail::to($this->argument('user'))->send(new UserRegistered());
+    }
+}
