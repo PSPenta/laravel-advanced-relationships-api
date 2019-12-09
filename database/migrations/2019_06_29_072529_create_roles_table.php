333d9c0fedd6ae79f740824e7b0a0d68de3f7391
Pritesh Shinde <priteshs@prdxn.com>
2019-12-09 08:41:18 +0530

    Database seeding implemented using Factories

--- database/migrations/2019_06_29_072529_create_roles_table.php
+++ database/migrations/2019_06_29_072529_create_roles_table.php
@@ -16,7 +16,7 @@ class CreateRolesTable extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('type');
-            $table->string('decription');
+            $table->string('description');
             $table->timestamps();
         });
     }
