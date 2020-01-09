de576541017b9792ac5f45eb9ecb9b5536b739a2
Pritesh Shinde <priteshs@prdxn.com>
2020-01-09 19:31:47 +0530

    RouteServiceProvider modified for controller namespace separation of web and api Controllers

--- routes/api.php
+++ routes/api.php
@@ -15,12 +15,12 @@ use Illuminate\Http\Request;
 
 Route::middleware('api.cors')->group(function () {
     // public routes
-    Route::post('/login', 'Api\AuthController@login')->name('login.api');
-    Route::post('/register', 'Api\AuthController@register')->name('register.api');
+    Route::post('/login', 'AuthController@login')->name('login.api');
+    Route::post('/register', 'AuthController@register')->name('register.api');
 
     Route::get('/unauthenticated', function () {
         return response()->json(["error" => "Unauthenticated user!"], 401);
-    })->name('login.error');
+    })->name('unauthenticated');
 
     Route::get('/unauthorized', function () {
         return response()->json(["error" => "Unauthorized user!"], 403);
@@ -28,53 +28,53 @@ Route::middleware('api.cors')->group(function () {
 
     // private routes
     Route::middleware('auth:api')->group(function () {
-        Route::get('/logout', 'Api\AuthController@logout')->name('logout');
-        Route::post('/change-password', 'Api\AuthController@changePassword')->name('change.password');
+        Route::get('/logout', 'AuthController@logout')->name('logout');
+        Route::post('/change-password', 'AuthController@changePassword')->name('change.password');
 
         Route::prefix('users')->group(function () {
-            Route::get('/', 'Api\UserController@getUsers');
-            Route::get('/paginate', 'Api\UserController@getUsersPaginate');
-            Route::get('/{id}', 'Api\UserController@getUser');
-            Route::post('/', 'Api\UserController@addUser');
-            Route::put('/{id}', 'Api\UserController@updateUser');
-            Route::delete('/{id}', 'Api\UserController@deleteUser');
+            Route::get('/', 'UserController@getUsers');
+            Route::get('/paginate', 'UserController@getUsersPaginate');
+            Route::get('/{id}', 'UserController@getUser');
+            Route::post('/', 'UserController@addUser');
+            Route::put('/{id}', 'UserController@updateUser');
+            Route::delete('/{id}', 'UserController@deleteUser');
         });
 
         Route::prefix('students')->group(function () {
-            Route::get('/', 'Api\StudentController@getStudents');
-            Route::get('/{id}', 'Api\StudentController@getStudent');
-            Route::post('/', 'Api\StudentController@addStudent');
-            Route::put('/{id}', 'Api\StudentController@updateStudent');
-            Route::delete('/{id}', 'Api\StudentController@deleteStudent');
-            Route::post('/f', 'Api\StudentController@addStudentFillable');
-            Route::put('/{id}/f', 'Api\StudentController@updateStudentFillable');
+            Route::get('/', 'StudentController@getStudents');
+            Route::get('/{id}', 'StudentController@getStudent');
+            Route::post('/', 'StudentController@addStudent');
+            Route::put('/{id}', 'StudentController@updateStudent');
+            Route::delete('/{id}', 'StudentController@deleteStudent');
+            Route::post('/f', 'StudentController@addStudentFillable');
+            Route::put('/{id}/f', 'StudentController@updateStudentFillable');
 
-            Route::delete('/{id}/multiple', 'Api\StudentController@deleteMultipleStudents');
-            Route::delete('/', 'Api\StudentController@deleteAllStudents');
-            Route::get('/{id}/s', 'Api\StudentController@getSoftDeletedStudent');
-            Route::put('/{id}/sr', 'Api\StudentController@restoreSoftDeletedStudent');
-            Route::delete('/{id}/force', 'Api\StudentController@forceDeleteSoftDeletedStudent');
+            Route::delete('/{id}/multiple', 'StudentController@deleteMultipleStudents');
+            Route::delete('/', 'StudentController@deleteAllStudents');
+            Route::get('/{id}/s', 'StudentController@getSoftDeletedStudent');
+            Route::put('/{id}/sr', 'StudentController@restoreSoftDeletedStudent');
+            Route::delete('/{id}/force', 'StudentController@forceDeleteSoftDeletedStudent');
         });
 
-        Route::get('/all-students', 'Api\EloquentRelationshipsController@getStudentsWithAllData');
-        Route::get('/students/{id}/oto', 'Api\EloquentRelationshipsController@getStudentOneToOne');
-        Route::get('/subjects/{id}/oto', 'Api\EloquentRelationshipsController@getSubjectOneToOne');
-        Route::post('/subjects/{id}/oto', 'Api\EloquentRelationshipsController@setSubjectOneToOne');
-        Route::put('/subjects/{id}/oto', 'Api\EloquentRelationshipsController@updateSubjectOneToOne');
-        Route::delete('/subjects/{id}/oto', 'Api\EloquentRelationshipsController@deleteSubjectOneToOne');
-        Route::get('/subjects/{id}/otm', 'Api\EloquentRelationshipsController@getSubjectOneToMany');
-        Route::post('/subjects/{id}/otm', 'Api\EloquentRelationshipsController@setSubjectOneToMany');
-        Route::put('/subjects/{id}/otm', 'Api\EloquentRelationshipsController@updateSubjectOneToMany');
-        Route::delete('/subjects/{id}/otm', 'Api\EloquentRelationshipsController@deleteSubjectOneToMany');
-        Route::get('/users/{id}/mtm', 'Api\EloquentRelationshipsController@getRolesManyToMany');
-        Route::post('/users/mtm/{rid}/{uid}', 'Api\EloquentRelationshipsController@setRolesManyToMany');
-        Route::delete('/users/{id}/mtm', 'Api\EloquentRelationshipsController@deleteRolesManyToMany');
-        Route::get('/roles/{id}/mtm', 'Api\EloquentRelationshipsController@getUsersManyToMany');
-        Route::get('/roles/{id}/hmt', 'Api\EloquentRelationshipsController@getRolesHasManyThrough');
-        Route::get('/photos/{id}/poly', 'Api\EloquentRelationshipsController@getPhotosPolymorphic');
-        Route::get('/photos/{id}/owner/poly', 'Api\EloquentRelationshipsController@getPhotosOwnerPolymorphic');
-        Route::get('/tags/{id}/polym2m', 'Api\EloquentRelationshipsController@getTagsPolymorphic');
-        Route::get('/tags/{id}/owner/polym2m', 'Api\EloquentRelationshipsController@getTagsOwnerPolymorphic');
+        Route::get('/all-students', 'EloquentRelationshipsController@getStudentsWithAllData');
+        Route::get('/students/{id}/oto', 'EloquentRelationshipsController@getStudentOneToOne');
+        Route::get('/subjects/{id}/oto', 'EloquentRelationshipsController@getSubjectOneToOne');
+        Route::post('/subjects/{id}/oto', 'EloquentRelationshipsController@setSubjectOneToOne');
+        Route::put('/subjects/{id}/oto', 'EloquentRelationshipsController@updateSubjectOneToOne');
+        Route::delete('/subjects/{id}/oto', 'EloquentRelationshipsController@deleteSubjectOneToOne');
+        Route::get('/subjects/{id}/otm', 'EloquentRelationshipsController@getSubjectOneToMany');
+        Route::post('/subjects/{id}/otm', 'EloquentRelationshipsController@setSubjectOneToMany');
+        Route::put('/subjects/{id}/otm', 'EloquentRelationshipsController@updateSubjectOneToMany');
+        Route::delete('/subjects/{id}/otm', 'EloquentRelationshipsController@deleteSubjectOneToMany');
+        Route::get('/users/{id}/mtm', 'EloquentRelationshipsController@getRolesManyToMany');
+        Route::post('/users/mtm/{rid}/{uid}', 'EloquentRelationshipsController@setRolesManyToMany');
+        Route::delete('/users/{id}/mtm', 'EloquentRelationshipsController@deleteRolesManyToMany');
+        Route::get('/roles/{id}/mtm', 'EloquentRelationshipsController@getUsersManyToMany');
+        Route::get('/roles/{id}/hmt', 'EloquentRelationshipsController@getRolesHasManyThrough');
+        Route::get('/photos/{id}/poly', 'EloquentRelationshipsController@getPhotosPolymorphic');
+        Route::get('/photos/{id}/owner/poly', 'EloquentRelationshipsController@getPhotosOwnerPolymorphic');
+        Route::get('/tags/{id}/polym2m', 'EloquentRelationshipsController@getTagsPolymorphic');
+        Route::get('/tags/{id}/owner/polym2m', 'EloquentRelationshipsController@getTagsOwnerPolymorphic');
     });
     Route::any('/{any}', function () {
         return response()->json(["error" => "Not found!"], 404);
