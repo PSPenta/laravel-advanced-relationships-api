a5ac0ba6ba4106c32d421a816c98dccdb9e48a65
Pritesh Shinde <priteshs@prdxn.com>
2020-02-15 23:22:35 +0530

    MySQL and MongoDB databases connected with Laravel at same time

--- app/Models/MongoDB/User.php
+++ app/Models/MongoDB/User.php
@@ -6,5 +6,17 @@ use Jenssegers\Mongodb\Eloquent\Model;
 
 class User extends Model
 {
+    /**
+     * The attribute to notify the connection type.
+     *
+     * @var string
+     */
+    protected $connection = 'mongodb';
+
+    /**
+     * The collection name.
+     *
+     * @var string
+     */
     protected $collection = 'users';
 }
