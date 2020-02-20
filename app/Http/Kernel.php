cda711829b1c2fe5db1d7e18a5278192b6fa6c43
Pritesh Shinde <priteshs@prdxn.com>
2020-02-20 22:31:01 +0530

    Auth0 implemented for API authentication

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'api.cors' => \App\Http\Middleware\Cors::class,
+        'jwt' => \App\Http\Middleware\CheckJWT::class,
     ];
 
     /**
---
+++ app/Http/Middleware/CheckJWT.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth0\SDK\JWTVerifier;
+use Closure;
+
+class CheckJWT
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param string $scopeRequired - Scope to check for.
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $scopeRequired = null)
+    {
+        $accessToken = $request->bearerToken();
+        if (empty($accessToken)) {
+            return response()->json(['message' => 'Bearer token missing'], 401);
+        }
+
+        $laravelConfig = config('laravel-auth0');
+        $jwtConfig = [
+            'authorized_iss' => $laravelConfig['authorized_issuers'],
+            'valid_audiences' => [$laravelConfig['api_identifier']],
+            'supported_algs' => $laravelConfig['supported_algs'],
+        ];
+
+        try {
+            $jwtVerifier = new JWTVerifier($jwtConfig);
+            $decodedToken = $jwtVerifier->verifyAndDecode($accessToken);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 401);
+        }
+
+        if ($scopeRequired && !$this->tokenHasScope($decodedToken, $scopeRequired)) {
+            return response()->json(['message' => 'Insufficient scope'], 403);
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * Check if a token has a specific scope.
+     *
+     * @param \stdClass $token - JWT access token to check.
+     * @param string $scopeRequired - Scope to check for.
+     *
+     * @return bool
+     */
+    protected function tokenHasScope($token, $scopeRequired)
+    {
+        if (empty($token->scope)) {
+            return false;
+        }
+
+        $tokenScopes = explode(' ', $token->scope);
+        return in_array($scopeRequired, $tokenScopes);
+    }
+}
